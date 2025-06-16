c3d74300ce664fa2680ec5c0afecc8d0ed38cdc9
Daniils <petrovs.danila20@gmail.com>
2025-06-16 13:22:45 +0300

    Update layout with metadata and SEO head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,3 @@
-"use client";
-// import { useState } from "react";
-
-// import "./globals.css";
-
-// import { LangProvider } from "./context/LangContext";
-
-// export default function RootLayout({
-//     children,
-// }: Readonly<{
-//     children: React.ReactNode;
-// }>) {
-//     const [lagn, setLang] = useState("ru");
-
-//     return (
-//         <html lang="en">
-//             <body>
-//                 <LangProvider>{children}</LangProvider>
-//             </body>
-//         </html>
-//     );
-// }
-
-
-// app/layout.tsx
-
-"use client";
 
 import "./globals.css";
 import { ReactNode } from "react";
