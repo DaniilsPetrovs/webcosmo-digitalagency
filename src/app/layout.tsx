5e7f34ac6fa9deba94280a0905445824cff195eb
Daniils <petrovs.danila20@gmail.com>
2025-06-16 12:24:13 +0300

    Update layout with metadata and SEO head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,72 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
+
+// import "./globals.css";
+
+// import { LangProvider } from "./context/LangContext";
+
+// export default function RootLayout({
+//     children,
+// }: Readonly<{
+//     children: React.ReactNode;
+// }>) {
+//     const [lagn, setLang] = useState("ru");
+
+//     return (
+//         <html lang="en">
+//             <body>
+//                 <LangProvider>{children}</LangProvider>
+//             </body>
+//         </html>
+//     );
+// }
+
+
+// app/layout.tsx
+
+"use client";
 
 import "./globals.css";
-
+import { ReactNode } from "react";
 import { LangProvider } from "./context/LangContext";
+import type { Metadata } from "next";
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
-    const [lagn, setLang] = useState("ru");
+export const metadata: Metadata = {
+  title: "WebCosmo - Digital Agency",
+  description:
+    "Webcosmo is a leading Web and Web3 development agency specializing in cutting-edge websites, blockchain solutions, and smart contract development.",
+  keywords:
+    "web development, web3 development, blockchain, digital agency, smart contract development, decentralized applications, creative design, UI/UX, Webcosmo",
+  authors: [{ name: "Webcosmo" }],
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.png",
+    apple: "/favicon.png",
+  },
+  manifest: "/site.webmanifest",
+  openGraph: {
+    title: "WebCosmo - Digital Agency",
+    description:
+      "Webcosmo offers innovative web and Web3 development solutions, from responsive websites to blockchain integrations and dApp development.",
+    url: "https://thewebcosmo.com",
+    type: "website",
+    images: [
+      {
+        url: "https://thewebcosmo.com/icon.png",
+        width: 1200,
+        height: 630,
+        alt: "WebCosmo Thumbnail",
+      },
+    ],
+  },
+};
 
-    return (
-        <html lang="en">
-            <body>
-                <LangProvider>{children}</LangProvider>
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <LangProvider>{children}</LangProvider>
+      </body>
+    </html>
+  );
 }
